d7aa30eb9f01143bd5dc782a374baea61e26bc3d
Actiproj <info@actiproj.com>
2021-11-25 16:35:41 +0100

    Ajoutez des fichiers projet.

---
+++ TestSession/Program.cs
@@ -0,0 +1,39 @@
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using TestSession;
+using TestSession.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<MenuInformation>();
+
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddSession();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ TestSession/SessionUser.cs
@@ -0,0 +1,15 @@
+﻿namespace TestSession
+{    public class SessionUser
+    {
+        public Boolean Complete { get; set; } = false;
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class MenuInformation
+    {
+        public event Action OnChange;
+        public void NotifyDataChanged() => OnChange?.Invoke();
+    }
+}
